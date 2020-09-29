a748c45f2acda003df1023b49bce6ec17b3dc426
Martynas Kasparavicius <martis.kasparavicius@gmail.com>
2020-09-30 00:35:01 +0300

    header added

---
+++ src/components/Header/header.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="Header__Container md-layout">
+        <div class="Header__Image md-layout-item md-size-50">
+            <img
+                alt="Profile picture"
+                src="profile_default.png"
+            />
+        </div>
+        <div class="Header__Info md-layout-item md-size-50">
+            <div class="md-layout-item md-size-100">
+                <h1>Martynas Kasparavičius</h1>
+            </div>
+            <div class="Header__Socials md-layout-item md-size-100">
+                <div class="Header__SocialItem">
+                    <a target="_blank" href="#">
+                        <i class="fab fa-github"></i> MartynasKasp
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .Header__Container {
+        height: auto;
+        width: 100%;
+        background-color: hsl(191deg, 56%, 58%);
+        color: white;
+        padding: 16px;
+
+        .Header__Image {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            padding-right: 16px;
+
+            img {
+                border-radius: 50%;
+                border: 1px solid white;
+                height: 200px;
+                width: 200px;
+            }
+        }
+
+        .Header__Info {
+            padding-left: 16px;
+
+            h1 {
+                text-align: left;
+            }
+
+            .Header__Socials {
+                display: flex;
+
+                a {
+                    color: white;
+                    text-decoration: none;
+                    border-bottom: 1px dotted white;
+
+                    &:hover {
+                        border-bottom: none;
+                    }
+                }
+            }
+        }
+    }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import 'vue-material/dist/vue-material.min.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
