280fcba3939c718342f160710b825c8b825218a7
Martynas Kasparavicius <martis.kasparavicius@gmail.com>
2020-09-29 19:59:01 +0300

    vue app added

---
+++ client/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'no-plus-plus': 'off',
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
