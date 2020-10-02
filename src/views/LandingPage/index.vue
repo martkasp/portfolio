6a59fe263d44ff9e49b7ea60bad7a1ba07dcfe59
Martynas Kasparavicius <martis.kasparavicius@gmail.com>
2020-10-02 15:00:36 +0000

    eslint rules changes, examples

--- src/views/LandingPage/index.vue
+++ src/views/LandingPage/index.vue
@@ -1,10 +1,12 @@
 <template>
-    <Header />
+    <Header :title="fullName" />
 </template>
 
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component';
+import { Get } from '@/utils/vuex-module-mutators';
 import Header from '@/components/Header/header.vue';
+import landingPage from '@/modules/LandingPage';
 
 @Options({
     components: {
@@ -12,6 +14,12 @@ import Header from '@/components/Header/header.vue';
     },
 })
 export default class LandingPage extends Vue {
+    @Get(landingPage) private fullName!: string;
 
+    created() {
+        console.log('Default value', this.fullName);
+        landingPage.changeFullName('John Doe');
+        console.log('New value', this.fullName);
+    }
 }
 </script>
